2a859c3c89e24ce8a414c3026ba1eb8195e1a091
tdr680 <tdr680@yahoo.de>
2016-01-23 17:27:05 +0100

    Stanford - Developing iOS 8 Apps with Swift - 5. Objective C Compatibility, Property List, Views https://www.youtube.com/watch?v=UPDeXCbix3w

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Tomas on 23/01/16.
+//  Copyright © 2016 Codeafterwork. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
