da3e26b093f8f2c185ddabe510ffaece452ce5db
tdr680 <tdr680@yahoo.de>
2016-01-24 16:55:37 +0100

    Stanford - Developing iOS 8 Apps with Swift - 6. Protocols and Delegation, Gestures https://www.youtube.com/watch?v=AHx4nE7EMic

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -8,6 +8,46 @@
 
 import UIKit
 
-class HappinessViewController: UIViewController {
+class HappinessViewController: UIViewController, FaceViewDataSource {
 
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = sender.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                sender.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+    
+    var happiness: Int = 10 { // 0 = very sad, 100 = ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness: \(happiness)")
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50.0
+    }
 }
